b996d8f3ddd22ad291238c862ff1fcab90ccd5bf
Nader Salahi <cp3@live.de>
2018-01-02 10:48:51 +0100

    Änderung Verbindungsaufbau ohne OnlineServerliste

--- src/Server/ServerStub.java
+++ src/Server/ServerStub.java
@@ -17,11 +17,11 @@ public interface ServerStub extends Remote{
     
     public boolean initConnection(String ip) throws RemoteException;
     
-    public LinkedList<String> getOnlineServerList() throws RemoteException;   
+    
     
     public boolean ping() throws RemoteException;   
     public boolean isServerReachable(String ip) throws RemoteException;
     
-    public void updateOnlineServerList(String neueIP, String senderIP) throws RemoteException;
+    
     public void entferneServerAusSystem(String serverIP, String senderIP) throws RemoteException;
 }
